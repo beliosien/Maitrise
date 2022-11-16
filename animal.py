929658fb21f000aed344609d43bc13567e2ec08b
beliosien <beliasossim@gmail.com>
2022-11-16 17:08:40 -0500

    Dog class added

--- animal.py
+++ animal.py
@@ -2,3 +2,12 @@ class Animal:
     def __init__(self, name: str, n_legs: int) -> None:
         self.name = name
         self.n_legs = n_legs
+
+
+class Dog(Animal):
+
+    def __init__(self, name: str, n_legs: int) -> None:
+        super().__init__(name, n_legs)
+
+    def run(self):
+        print("Woof Woof !")
