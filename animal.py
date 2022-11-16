4b548c7d0535cb9229b95fe8fb7138a1bb3300f0
beliosien <beliasossim@gmail.com>
2022-11-16 16:52:15 -0500

    Turning the class into an abstract class

--- animal.py
+++ animal.py
@@ -1,4 +1,12 @@
-class Animal:
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
     def __init__(self, name: str, n_legs: int) -> None:
         self.name = name
         self.n_legs = n_legs
+
+
+    @abstractmethod
+    def run(self) ->None:
+        pass
